ba645fa26e69f23d43830c8c7e4beed7fb25c7c8
Koroban <koroban@novutec.com>
2013-01-08 13:13:31 +0100

    changed layout to google like calendar. removed input support - standalone calendar. added highlight for current date and weekends. added support to set start of week.

--- jquery.datePicker.js
+++ jquery.datePicker.js
@@ -1,15 +1,12 @@
 (function($){
   $.fn.datePicker = function(opts) {
-
-    if (opts && opts.command == 'toggle') {
-      this.get(0).datePicker.toggle();
-      return this;
-    }
-
-    var defaults = {selected: null, minimumDate: null, maximumDate: null};
+    var defaults = {selected: null, minimumDate: null, maximumDate: null, firstDay: 0};
     var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
     var abbreviations = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec');
+    var days = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
     var daySelector = 'td:not(.m):not(:empty)';
+    var data = {};
+    var today = new Date();
 
     return this.each(function() {
       if (this.datePicker) { return false; }
@@ -19,8 +16,7 @@
       var self = {
         initialize: function() {
           $input.click(function (event) {self.show(); return false;}).keydown(function(e){ if (e.keyCode == 13) { self.entered(); return false; }});
-          $(document).keydown(function(e) { if (e.keyCode == 27) { self.hide(); }}).click(self.hide);
-          $container = self.initializeContainer().hide()
+          $container = self.initializeContainer()
             .append(self.buildMonth(new Date()))
             .delegate(daySelector, 'click', self.clicked)
             .delegate('td:not(:empty)', 'hover', self.hover)
@@ -31,9 +27,6 @@
             .click(function(){return false;});
         },
         parseDate: function(value) { return new Date(value); },
-        toggle: function() { $container.is(':visible') ? self.hide() : self.show(); },
-        show: function() { $container.show(); },
-        hide: function() { $container.hide(); },
         loadPrevious: function() {
           $container.empty().append(self.buildMonth(new Date(currentDate.getFullYear(), currentDate.getMonth()-1, 1)));
         },
@@ -54,18 +47,13 @@
             $container.find('td.selected').removeClass('selected');
             $cell.addClass('selected');
             var date = new Date(currentDate.getFullYear(), currentDate.getMonth(), $cell.text());
-            $input.val(self.format(date)).change();
             if (options.selected != null) { options.selected(date, $cell); }
-            self.hide();
           }
         },
-        entered: function() {
-          var date = self.parseDate($input.val().replace(/^\s*/, '').replace(/\s*$/, ''));
-          if (date == null) { return; }
-          $container.empty().append(self.buildMonth(date)).find(daySelector).eq(date.getDate()-1).click();
-        },
         initializeContainer: function() {
-          return $('<div>').addClass('calendar').insertAfter($input);
+            x=$('<div>').addClass('calendar');
+            $input.html(x);
+            return x;
         },
         pickYear: function() {
           mode = 'year';
@@ -91,6 +79,7 @@
           year.innerHTML = currentDate.getFullYear();
 
           //could make this static...
+          // but you should not - cause we can not dynamicly change month name anymore...
           for (var i = 0; i < 3; ++i) {
             var row = table.insertRow(-1);
             for (var j = 0; j < 4; ++j) {
@@ -102,20 +91,42 @@
         },
         buildMonth: function(date) {
           mode = 'day';
-          var first = new Date(date.getFullYear(), date.getMonth(), 1);
+          var first = new Date(date.getFullYear(), date.getMonth(), 2 - options.firstDay );
           var last = new Date(date.getFullYear(), date.getMonth()+1, 0);
           currentDate = first;
           var firstDay = first.getDay();
           var totalDays = last.getDate();
           var weeks = Math.ceil((totalDays + firstDay) / 7);
           var table = document.createElement('table');
+          var hl = (today.getFullYear() == date.getFullYear() && today.getMonth() == date.getMonth()) ? today.getDate() : undefined;
+
+          var row = table.insertRow(-1);
+          row.className = 'head';
+          weekend_cols = new Array();
+          for(var i = 0, count = 0; count < 7; count++, i++) {
+            var cell = row.insertCell(-1);
+            if (i+options.firstDay > days.length-1) {
+              i = options.firstDay * -1;
+            }
+            offset = i+options.firstDay;
+            if (offset == 0 || offset == 6) {
+                weekend_cols.push(count)
+            }
+            cell.innerHTML = days[offset];
+          }
 
           for (var i = 0, count = 1; i < weeks; ++i) {
             var row = table.insertRow(-1);
-            for(var j = 0; j < 7; ++j, ++count) {
+            for (var j = 0; j < 7; ++j, ++count) {
               var cell = row.insertCell(-1);
-              if (count > firstDay && count <= totalDays+firstDay) {
-                cell.innerHTML = count - firstDay;
+              if (count > firstDay && count <= totalDays + firstDay) {
+                vl = count - firstDay;
+                if (hl && hl == vl) {
+                  cell.className = 'today';
+                } else if (j == weekend_cols[0] || j == weekend_cols[1]) {
+                  cell.className = 'weekend';
+                }
+                cell.innerHTML = vl;
               }
             }
           }
